b0831e2ba1ba1344fa28babbc8d23ac86cb82c62
Felipe <felipesouzagomes21@gmail.com>
2024-10-03 21:09:51 -0300

    chore: start user-server-api

---
+++ user-server-api/src/test/java/br/com/felipe/userserverapi/UserServerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.felipe.userserverapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
