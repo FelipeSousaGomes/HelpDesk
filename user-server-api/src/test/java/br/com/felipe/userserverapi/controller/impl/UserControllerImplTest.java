e156b766c1aa921e1780dd101dab025f884cb974
felip <felipesouzagomes21@gmail.com>
2025-12-06 22:50:49 -0300

    feat : add test integration test to find by id User Controller

---
+++ user-server-api/src/test/java/br/com/felipe/userserverapi/controller/impl/UserControllerImplTest.java
@@ -0,0 +1,48 @@
+package br.com.felipe.userserverapi.controller.impl;
+
+import br.com.felipe.userserverapi.entity.User;
+import br.com.felipe.userserverapi.repository.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static br.com.felipe.userserverapi.creator.CreatorUtils.generatedMock;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+class UserControllerImplTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private UserRepository userRepository;
+
+
+    @Test
+    void testFindByIdWithSuccess() throws Exception {
+        final var entity = generatedMock(User.class);
+        final var userId = userRepository.save(entity).getId();
+
+        mockMvc.perform(get("/api/users/{id}", userId))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(userId))
+                .andExpect(jsonPath("$.name").value(entity.getName()))
+                .andExpect(jsonPath("$.email").value(entity.getEmail()))
+                .andExpect(jsonPath("$.password").value(entity.getPassword()))
+                .andExpect(jsonPath("$.profiles").isArray());
+
+        userRepository.deleteById(userId);
+    }
+
+
+}
