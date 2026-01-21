c0e4faeaa57057eed03cfd1ae339c7b44ab3774d
felip <felipesouzagomes21@gmail.com>
2026-01-21 15:22:35 -0300

    chore: start order service api

---
+++ oder-service-api/src/main/java/br/com/felipe/oderserviceapi/OderServiceApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.felipe.oderserviceapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OderServiceApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OderServiceApiApplication.class, args);
+    }
+
+}
---
+++ oder-service-api/src/test/java/br/com/felipe/oderserviceapi/OderServiceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.felipe.oderserviceapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OderServiceApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
