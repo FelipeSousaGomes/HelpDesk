201453329818aafb47fba07f8204e37f454a83ac
felip <felipesouzagomes21@gmail.com>
2026-01-22 13:56:16 -0300

    feat : add create order request structure

---
+++ hd-commons-lib/src/main/java/models/request/CreateOrderRequest.java
@@ -0,0 +1,34 @@
+package models.request;
+
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record CreateOrderRequest(
+
+        @Schema(description = "Requester id", example = "694c8c9fd49bc03503bd6aa1")
+        @NotBlank(message = "Requester id cannot be empty")
+        @Size(min = 24, max = 36, message = "Invalid requester id")
+        String requesterId,
+
+        @Schema(description = "Requester id", example = "694c8c9fd49bc03503bd6aa1")
+        @NotBlank(message = "Customer id cannot be empty")
+        @Size(min = 24, max = 36, message = "Invalid customer id")
+        String customerId,
+
+        @Schema(description = "Title of Order", example = "Fix my Computer")
+        @NotBlank(message = "Title id cannot be empty")
+        @Size(min = 3, max = 45, message = "The title must be between 3 and 45 characters")
+        String title,
+        @Schema(description = "Description of Order", example = "My computer is broken")
+        @NotBlank(message = "Description id cannot be empty")
+        @Size(min = 10, max = 3000, message = "The description must be between 10 and 3000 characters")
+        String description,
+
+        @Schema(description = "Status of Order", example = "Open")
+        @NotBlank(message = "Status id cannot be empty")
+        @Size(min = 4, max = 15, message = "The status must be between 4 and 15 characters")
+        String status
+) {
+}
